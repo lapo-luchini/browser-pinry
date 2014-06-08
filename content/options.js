7a1a6ed4d08deb1e79d14e7a23a638c115789c62
Lapo Luchini <lapo@lapo.it>
2014-06-08 10:35:26 +0000

    First working version.

---
+++ content/options.js
@@ -0,0 +1,18 @@
+﻿// Show and save changes to browser extension settings.
+
+var defaultURL = 'http://demo.getpinry.com/',
+    input = document.getElementById('pinryUrl'),
+    re = /[/]+(static[/]js[/]bookmarklet[.]js)?$/;
+
+function load() {
+  chrome.storage.local.get('pinryUrl', function (obj) {
+    input.value = (obj.pinryUrl || defaultURL).replace(re, '');
+  });
+}
+
+function save() {
+  chrome.storage.local.set({ pinryUrl: input.value }, load);
+}
+
+input.addEventListener('change', save);
+document.addEventListener('DOMContentLoaded', load);
