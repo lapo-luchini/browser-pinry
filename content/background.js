5700aa400469b782c67470a0bc024fa0367b28ef
winkidney <winkidney@gmail.com>
2018-08-19 20:51:03 +0800

    Feature: Add context menu for pinry

--- content/background.js
+++ content/background.js
@@ -1,3 +1,15 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.executeScript(tab.id, { file: 'bookmarklet.js' })
 });
+
+chrome.contextMenus.create(
+    {
+      "title": "Run Pinry",
+      "contexts": [
+        "page","selection","link","editable","image","video", "audio"
+      ],
+      "onclick": function (info, tab) {
+         chrome.tabs.executeScript(tab.id, { file: 'bookmarklet.js' })
+      }
+    }
+);
