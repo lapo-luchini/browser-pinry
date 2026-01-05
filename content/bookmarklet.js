87e5de2dc0632e73a8cb37c5481f973293d4fe5f
Lapo Luchini <lapo@lapo.it>
2026-01-05 20:11:53 +0000

    Use Prettier to re-format code.

--- content/bookmarklet.js
+++ content/bookmarklet.js
@@ -1,17 +1,18 @@
-void((function (d) {
-    var prev = d.getElementById('pinry-bookmarklet');
-    if (prev) // remove previous instance
+void (function (d) {
+    var prev = d.getElementById('pinry-bookmarklet')
+    if (prev)
+        // remove previous instance
         try {
-            prev.remove();
-            d.getElementById('pinry-images').remove();
+            prev.remove()
+            d.getElementById('pinry-images').remove()
         } finally {
-            return;
+            return
         }
     // load new instance
     chrome.storage.local.get('pinryUrl', function (obj) {
-        var s = d.createElement('script');
-        s.id  = 'pinry-bookmarklet';
-        s.src = obj.pinryUrl + '/static/js/bookmarklet.js?' + Math.random() * 10000000000000000;
-        d.body.appendChild(s);
-    });
-})(document));
+        var s = d.createElement('script')
+        s.id = 'pinry-bookmarklet'
+        s.src = obj.pinryUrl + '/static/js/bookmarklet.js?' + Math.random() * 10000000000000000
+        d.body.appendChild(s)
+    })
+})(document)
--- content/options.js
+++ content/options.js
@@ -1,52 +1,53 @@
 // Show and save changes to browser extension settings.
 
-const
-    defaultURL = 'http://demo.getpinry.com/',
+const defaultURL = 'http://demo.getpinry.com/',
     url = document.getElementById('pinryUrl'),
     menu = document.getElementById('contextMenu'),
-    re = /[/]+(static[/]js[/]bookmarklet[.]js)?$/;
+    re = /[/]+(static[/]js[/]bookmarklet[.]js)?$/
 
 function checkIMG(url) {
     return new Promise((resolve, reject) => {
         // we can avoid extra permissions and CORS this way
-        const img = new Image();
-        img.onload = resolve;
-        img.onerror = reject;
-        img.src = url;
-    });
+        const img = new Image()
+        img.onload = resolve
+        img.onerror = reject
+        img.src = url
+    })
 }
 
 function check() {
-    const img1 = checkIMG(url.value + '/static/img/logo-dark.png'); // Pinry pre-SPA
-    const img2 = checkIMG(url.value + '/img/icons/android-chrome-192x192.png'); // Pinry SPA
-    Promise.any([img1, img2]).then(() => {
-        url.style.backgroundColor = '#9F9'
-    }).catch(e => {
-        console.log(e.errors)
-        url.style.backgroundColor = '#F99'
-    });
+    const img1 = checkIMG(url.value + '/static/img/logo-dark.png') // Pinry pre-SPA
+    const img2 = checkIMG(url.value + '/img/icons/android-chrome-192x192.png') // Pinry SPA
+    Promise.any([img1, img2])
+        .then(() => {
+            url.style.backgroundColor = '#9F9'
+        })
+        .catch((e) => {
+            console.log(e.errors)
+            url.style.backgroundColor = '#F99'
+        })
 }
 
 function load() {
     chrome.storage.local.get(null, function (obj) {
-        url.value = obj.pinryUrl || defaultURL;
-        menu.checked = (obj.contextMenu === false) ? false : true; // default true
-        check();
-    });
+        url.value = obj.pinryUrl || defaultURL
+        menu.checked = obj.contextMenu === false ? false : true // default true
+        check()
+    })
 }
 
 function save() {
-    url.value = url.value.trim().replace(re, '');
-    check();
+    url.value = url.value.trim().replace(re, '')
+    check()
     const cfg = {
         pinryUrl: url.value,
         contextMenu: menu.checked,
-    };
-    chrome.storage.local.set(cfg, load);
-    chrome.runtime.sendMessage(null, 'updateContextMenu');
-    console.log('Saved:', cfg);
+    }
+    chrome.storage.local.set(cfg, load)
+    chrome.runtime.sendMessage(null, 'updateContextMenu')
+    console.log('Saved:', cfg)
 }
 
-document.addEventListener('DOMContentLoaded', load);
-url.addEventListener('change', save);
-menu.addEventListener('change', save);
+document.addEventListener('DOMContentLoaded', load)
+url.addEventListener('change', save)
+menu.addEventListener('change', save)
